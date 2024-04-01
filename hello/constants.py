f59f979b6c2812ea769b4df0c0631fb6e0217775
KiloJon <social@kjon.life>
2024-03-31 20:49:30 -0500

    style: Add modal with appropriate styling

---
+++ hello/constants.py
@@ -0,0 +1,4 @@
+# constants.py
+
+MY_LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/jonhwilliams'
+MY_GITHUB_PROFILE_URL = 'https://github.com/kjon-life'
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .constants import MY_LINKEDIN_PROFILE_URL, MY_GITHUB_PROFILE_URL
 
 # Create your views here.
 def index(request):
-    return render(request, 'hello/index.html')
+    context = {
+        'MY_LINKEDIN_PROFILE_URL': MY_LINKEDIN_PROFILE_URL,
+        'MY_GITHUB_PROFILE_URL': MY_GITHUB_PROFILE_URL,
+    }
+    return render(request, 'hello/index.html', context)
